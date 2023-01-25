4f6122693649ab326825793dd30c744558831928
leviayelin <leviwasa@gmail.com>
2023-01-25 03:39:31 +0200

    uploading website

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+//Selectors Section 
+//creating variable for show and hide button
+const hemburger = document.querySelectorAll(".hamburger");
+const showNavbar = document.querySelector(".navbar");
+
+//Functions Section
+function navToggle(){
+//toggle between the navbar hide and show	
+	showNavbar.classList.toggle("show");
+}
+
+//Event Listener Section
+for(let i = 0 ; i < hemburger.length; i++){
+// useing for loop that will able me to 
+//use the button as much as needed 	
+hemburger[i].addEventListener("click",navToggle);
+}
